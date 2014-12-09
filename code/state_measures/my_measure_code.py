f6777758b03d7733f46e220ad775c3020b209b92
Philip Janisiewicz <pjanisiewicz@gmail.com>
2014-12-09 15:39:50 +0100

    initial analysis

---
+++ code/state_measures/my_measure_code.py
@@ -0,0 +1,16 @@
+
+import funtool.state_measure
+import numbers
+
+def all_ratios(state_measure, analysis_collection, state_collection, overriding_parameters=None):
+    measure_parameters = funtool.state_measure.get_measure_parameters(state_measure, overriding_parameters)
+    all_measures= list(analysis_collection.state.measures.keys())
+    for idx,first_measure in enumerate(all_measures):
+        if isinstance(analysis_collection.state.measures[first_measure],numbers.Number):
+            for second_measure in all_measures[(idx+1):-1]:
+                if isinstance(analysis_collection.state.measures[second_measure],numbers.Number):
+                    a_value= analysis_collection.state.measures[first_measure]
+                    b_value= analysis_collection.state.measures[second_measure]
+                    analysis_collection.state.measures[first_measure+"_to_"+second_measure]= funtool.state_measures.default._compute_ratio(a_value,b_value)
+    return analysis_collection
+            
---
+++ run_analyses.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python3
+
+import funtool.api
+
+funtool.api.run_analyses()
