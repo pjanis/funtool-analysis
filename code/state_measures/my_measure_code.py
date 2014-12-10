20ff0fb9a6e417809eb6369467cf998762df155a
Philip Janisiewicz <pjanisiewicz@gmail.com>
2014-12-10 19:53:56 +0100

    added decorated measure function example to code and analysis

--- code/state_measures/my_measure_code.py
+++ code/state_measures/my_measure_code.py
@@ -13,4 +13,17 @@ def all_ratios(state_measure, analysis_collection, state_collection, overriding_
                     b_value= analysis_collection.state.measures[second_measure]
                     analysis_collection.state.measures[first_measure+"_to_"+second_measure]= funtool.state_measures.default._compute_ratio(a_value,b_value)
     return analysis_collection
-            
+
+@funtool.state_measure.state_and_parameter_measure
+def number_of_scripts(state,parameters):
+    return len(state_scripts(state))
+
+#duplicate of function in funtool_scratch_processes.state_measures.default
+def state_scripts(state):   
+    json_data= state.data.get('json')
+    scripts=[]
+    if json_data != None:
+        scripts.extend(json_data.get('scripts',[]))
+        for child in json_data.get('children'):
+            scripts.extend(child.get('scripts',[]))
+    return scripts
